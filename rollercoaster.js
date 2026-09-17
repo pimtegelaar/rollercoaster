056a0d1996689dbd996f3ac34901f0f68c175ffa
Pim Tegelaar <pimtegelaar@users.noreply.github.com>
2026-09-17 22:24:51 +0200

    Reduce field of view for better realism

--- rollercoaster.js
+++ rollercoaster.js
@@ -21,7 +21,7 @@
   const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 1000);
   const zoom = {
     minFov: 25,
-    maxFov: 130,
+    maxFov: 100,
     wheelSensitivity: 0.025,
     pinchSensitivity: 0.12
   };
